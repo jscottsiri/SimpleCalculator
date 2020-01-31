7301ed18388c81957ffe31817bf44660e015236a
jscottsiri <jscottsiri@gmail.com>
2020-01-31 00:43:37 -0500

    Calculator with 4 functions and tests

---
+++ basiccalculator/calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace basiccalculator
+{
+    public class Calculator
+    {
+        static public int Add(int a, int b)
+        {
+            var c = a + b;
+
+            return c;
+        }
+        static public int Subtract(int a, int b)
+        {
+            var c = a - b;
+
+            return c;
+        }
+        static public int Multiply(int a, int b)
+        {
+            var c = a * b;
+        }
+        static public int Divide(int a, int b)
+        {
+            var c = a / b;
+        }
+
+    }
+}
---
+++ basiccalculatorTests/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using basiccalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basiccalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var c = Calculator.Add(1, 2);
+            Assert.AreEqual(3, c);
+
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            var c = Calculator.Subtract(1, 2);
+            Assert.AreEqual(-1, c);
+        }
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var c = Calculator.Multiply(3, 2);
+            Assert.AreEqual(6, c);
+        }
+        [TestMethod()]
+        public void SubtractDivide()
+        {
+            var c = Calculator.Divide(4, 2);
+            Assert.AreEqual(2, c);
+        }
+    }
+}
